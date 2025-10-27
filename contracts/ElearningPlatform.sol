1e36737dc6981e9bac1e07861c127b1520c26ebe
Thanh Dong <dongnt.22ite@vku.udn.vn>
2025-10-27 16:52:35 +0700

    add course registration logic

--- contracts/ElearningPlatform.sol
+++ contracts/ElearningPlatform.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 interface ICertificateNFT {
     function safeMint(
@@ -10,40 +10,48 @@ interface ICertificateNFT {
 
 contract ElearningPlatform {
     ICertificateNFT public certificateNFT;
-
     struct Course {
+        uint256 id;
         address instructor;
         uint256 price;
-        string courseId;
-        bool exists;
+        string title;
+        string contentCid;
     }
 
-    uint256 public nextCourseId;
+    uint256 public nextCourseId = 1;
 
     // Create a key (uint256) and value (Course) to track courses
     mapping(uint256 => Course) public courses;
 
-    // Track which students are enrolled in which courses
-    mapping(uint256 => mapping(address => bool)) public enrolled;
-
     // Create an event for course registration (an event is a log that can be listened to off-chain) to notify external systems (React app) when a new course is registered
-    event CourseRegistered(
+    event CourseCreated(
         uint256 indexed courseId,
         address indexed instructor,
+        string title,
         uint256 price,
-        string courseCID
+        string contentCid
     );
-    event Enrolled(
-        uint256 indexed courseId,
-        address indexed student,
-        uint256 amountPaid
-    );
-    event CertificateIssued(
-        uint256 indexed courseId,
-        address indexed student,
-        uint256 tokenId,
-        string certCID
-    );
+
+    // calldata means it can't not modify and don't copy into memory,
+    function createCourse(
+        string calldata title,
+        uint256 price,
+        string calldata contentCid
+    ) external returns (uint256 courseId) {
+        // courseId is a named return variable, means it's automatically initialized within the function's scope
+        require(bytes(title).length != 0, "Title is required");
+        require(bytes(contentCid).length != 0, "CID required");
+        courseId = nextCourseId++;
+        courses[courseId] = Course(
+            courseId,
+            msg.sender,
+            price,
+            title,
+            contentCid
+        );
+        // msg.sender is a global variable that represents the address of the account that called this function
+        emit CourseCreated(courseId, msg.sender, title, price, contentCid);
+    }
 
     constructor(address certificateNFTAddress) {
         require(certificateNFTAddress != address(0), "Invalid NFT address");
