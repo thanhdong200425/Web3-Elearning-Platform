87d534774bbd902ee593963434c454600a14d667
lam4est <lam058b@gmail.com>
2025-12-10 15:49:24 +0700

    feat: Update Hardhat configuration to use dotenv for environment variables and add deployment artifacts for CertificateNFT and ElearningPlatform contracts.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],
@@ -33,8 +33,8 @@ const config: HardhatUserConfig = {
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
       chainId: 11155111,
     },
   },
